e8aacbafaa6788090624484f68d5b70efcd1703d
Leonid <leonidbarsucovschi0503@gmail.com>
2020-02-11 16:37:24 +0200

    first task

---
+++ script.js
@@ -0,0 +1,9 @@
+// Первое задание
+//'use strict';
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt( "Введите дату в формате YYYY-MM-DD");
+    console.log(money);
+    console.log(time);
+
+    
+      
