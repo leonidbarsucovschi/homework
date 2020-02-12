f3edc40a9ae5584d61633de1dfead65afb8f41b8
Leonid <leonidbarsucovschi0503@gmail.com>
2020-02-12 13:17:46 +0200

    script modif

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Первое задание
-//'use strict';
+// Second task
+
 let money = prompt("Ваш бюджет на месяц?"),
     time = prompt( "Введите дату в формате YYYY-MM-DD");
     console.log(money);
     console.log(time);
 
-    // Второе задание 
+    // third task
 
-    let appData = [];
+    let appData = {
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        saving: false
+
+    };
+
+    
+
+    
 
     
       
